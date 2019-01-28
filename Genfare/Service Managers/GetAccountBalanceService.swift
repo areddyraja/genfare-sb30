72b3daca96b30206adf1aea5619559e60ba0ed89
Shravan Kumar Damera <ShravanKumar.Damera@spx.com>
2019-01-28 17:54:19 +0530

    servicesadded

---
+++ Genfare/Service Managers/GetAccountBalanceService.swift
@@ -0,0 +1,30 @@
+//
+//  GetAccountBalanceService.swift
+//  Genfare
+//
+//  Created by omniwyse on 28/01/19.
+//  Copyright © 2019 Omniwyse. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+class GFAccountBalanceService{
+    
+    init(){}
+    
+    func fetchAccountBalance(completionHandler:@escaping (_ success:Bool?,_ error:Any?) -> Void) {
+        let endpoint = GFEndpoint.GetAccountBalance()
+        
+        Alamofire.request(endpoint.url, method: endpoint.method, parameters: endpoint.parameters, encoding: URLEncoding.default, headers: endpoint.headers)
+            .responseJSON { response in
+                switch response.result {
+                case .success(let JSON):
+                    print(JSON)
+                case .failure(let error):
+                    print("Request failed with error: \(error)")
+                    completionHandler(false,error)
+                }
+        }
+    }
+    
+}

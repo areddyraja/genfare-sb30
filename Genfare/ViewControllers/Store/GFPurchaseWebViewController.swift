980b71aefaa812f0185c6801a14664e2209e1eb3
Shravan Kumar Damera <ShravanKumar.Damera@spx.com>
2019-02-07 13:25:56 +0530

    purchase

--- Genfare/ViewControllers/Store/GFPurchaseWebViewController.swift
+++ Genfare/ViewControllers/Store/GFPurchaseWebViewController.swift
@@ -7,47 +7,97 @@
 //
 
 import UIKit
+import WebKit
 
-class GFPurchaseWebViewController: UIViewController,UIWebViewDelegate {
-
-    @IBOutlet var paymentWebview: UIWebView!
+class GFPurchaseWebViewController: GFBaseViewController,WKNavigationDelegate{
+    
     var weburl:String!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.paymentWebview.loadRequest(URLRequest.init(url: URL(string:weburl)!))
+        
+        
+        let paymentWebview = WKWebView()
+        paymentWebview.frame  = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        self.view.addSubview(paymentWebview)
+        
+        
+        let token:String = KeychainWrapper.standard.string(forKey: Constants.KeyChain.SecretKey)!
+        let url =  self.weburl
+        var request = URLRequest(url: URL.init(string: url!)!)
+        request.setValue(String(format: "bearer %@", token), forHTTPHeaderField: "Authorization")
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue(Utilities.appCurrentVersion(), forHTTPHeaderField: "app_version")
+        request.setValue("iOS", forHTTPHeaderField: "app_os")
+        request.setValue(Utilities.deviceId(), forHTTPHeaderField: "DeviceId")
+        paymentWebview.load(request)
+        //        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        //        paymentWebview.delegate = self
         // Do any additional setup after loading the view.
     }
     
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
-    func webViewDidStartLoad(_ webView: UIWebView) {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-     //   appDelegate.showloader()
+    // MARK: WKWebview Delegate Methods
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void){
+        
+        let request = navigationAction.request
+        let urlString = request.url?.absoluteString
+        guard let finalWeburl = weburl else{
+            return
+        }
+        if (finalWeburl != urlString && !(urlString?.contains("checkout"))!) && !(urlString?.contains("coocoo://"))!{
+            
+        }
+        if (urlString?.hasPrefix("coocoo://"))!{
+            
+        }else if((urlString?.contains("finished"))!){
+            //Need to
+        }else if((urlString?.contains("Thank"))!){
+            navigationController?.popToRootViewController(animated: true)
+            
+        }
+        
+    }
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!){
+        
+        let urlString = webView.url?.absoluteString
+        if (urlString?.contains("purchase/proxy_form"))!{
+            //Do progress dialog
+            self.attachSpinner(value: true)
+            
+        }
+    }
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error){
+        self.attachSpinner(value: false)
+    }
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!){
+        
     }
     
-    func webViewDidFinishLoad(_ webView: UIWebView)
-    {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-     //   appDelegate.hideloader()
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!){
+        self.attachSpinner(value: false)
     }
-
-   
     
-
+    
+    
+    
+    
+    
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
